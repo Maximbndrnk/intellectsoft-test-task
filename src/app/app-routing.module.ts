9c80aaedec0333aabe3a05c7b312302c03d9e43e
Maxim Bondarenko <maksym.bon@sevencollab.com>
2021-07-04 16:32:43 +0300

    generate main and cart pages files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreComponent } from './store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/store',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: StoreComponent,
+    children: [
+      {
+        path: 'store',
+        loadChildren: () => import('./store/store.module').then((m) => m.StoreModule),
+      },
+    ]
+  },
+  {
+    path:'cart',
+    loadChildren: () => import('./cart/cart.module').then((m) => m.CartModule),
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
